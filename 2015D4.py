76d9e61873601fecb210f10440acc5ac68de3918
Allison <allie9926@yahoo.com.au>
2023-11-09 13:19:40 +1100

    Day 4 part 2 Solution

--- 2015D4.py
+++ 2015D4.py
@@ -1,3 +1,9 @@
 from hashlib import md5
 
 sKey = "yzbqklnj"
+
+for i in range (10000000000000000):
+    hashCode = md5((sKey + str(i)).encode()).hexdigest()
+    if hashCode[:6] == '000000':
+        print(i)
+        break
